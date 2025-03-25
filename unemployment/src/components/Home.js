6525f58c37b3f5b0761082f59a23e576b790759a
joshua <jo336@njit.edu>
2025-03-25 15:53:56 -0400

    Update Home.js

--- unemployment/src/components/Home.js
+++ unemployment/src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     return (
       <div>
         <h2>Unemployment Checker</h2>
-        <p>Welcome to the Unemployment Checker. Use the navigation to view data.</p>
+        <p>Welcome to the Unemployment Checker. In this app, I will analyze the rate of unemployment in America since the 1970's to create a hypothesis on the rise of unemployment in America.. Use the navigation to view data.</p>
         <Link to="csv1">
         <button>To Beginning</button>
       </Link>
